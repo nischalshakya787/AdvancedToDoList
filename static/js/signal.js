e8167c9491b1e6c45020b6b1b61df51f1d8a379f
nischalshakya787 <nischalshakya787@gmail.com>
2023-11-18 09:45:54 +0545

    Revert "Revert "Added FilterSystem""
    This reverts commit 2b57bccb57e6e762d9472d7ab54442014f2edd84.

--- static/js/signal.js
+++ static/js/signal.js
@@ -5,17 +5,34 @@ const titleDOM = document.querySelector("#inputTaskTitle");
 const descriptionDOM = document.querySelector("#inputTaskDescription");
 const colorDOM = document.querySelector("#colorSelection");
 const dateDOM = document.getElementById("displayDate");
+const filterBtn = document.querySelectorAll(".filterBtn");
 
-const showTask = async () => {
-  loadingDOM.style.visibility = "visible";
+const showTask = async (filterElement) => {
+  //filterElement is the button. From this button we will get the value which will determine the data to be filtered.
+  loadingDOM.style.display = "block";
+  filterBtn.forEach((element) => {
+    // This will remove all the active class from 3 buttons
+    element.classList.remove("active");
+  });
+
   try {
-    const {
-      data: { todo },
-    } = await axios.get("/api/tasks");
-
+    let todo;
+    if (filterElement.value === "all") {
+      const response = await axios.get("/api/tasks/all"); //api/tasks/:method
+      filterElement.classList.add("active");
+      todo = response.data.todo;
+    } else if (filterElement.value === "pending") {
+      const response = await axios.get("/api/tasks/pending"); //api/tasks/:method
+      filterElement.classList.add("active");
+      todo = response.data.todo;
+    } else {
+      const response = await axios.get("/api/tasks/completed"); //api/tasks/:method
+      filterElement.classList.add("active");
+      todo = response.data.todo;
+    }
     todo.sort((a, b) => new Date(a.date) - new Date(b.date)); //This will sort the date in ascending order
     if (todo.length < 1) {
-      loadingDOM.style.visibility = "hidden";
+      loadingDOM.style.display = "none";
       taskField.innerHTML = `<span>You have no Tasks</span>`;
       return;
     }
@@ -27,15 +44,21 @@ const showTask = async () => {
       })
       .join("");
     taskField.innerHTML = allTasks;
-    loadingDOM.style.visibility = "hidden";
+    loadingDOM.style.display = "none";
   } catch (error) {
     console.log(error);
     taskField.innerHTML = `<span>Error on Fetching the Data</span>`;
   }
-  loadingDOM.style.visibility = "hidden";
+  loadingDOM.style.display = "none";
 };
 
-showTask();
+showTask(filterBtn[0]);
+
+filterBtn.forEach((filterElement) => {
+  filterElement.addEventListener("click", () => {
+    showTask(filterElement);
+  });
+});
 
 formDOM.addEventListener("submit", async (e) => {
   // e.preventDefault();
